ff47519ed0230a6f9ea395e62bb5ba0c0f8bd4b3
[Silin] <[evsilin@yandex.ru]>
2021-04-28 12:08:59 +0300

    “lesson2”

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+# from mainapp.models import Product, ProductCategory
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'mainapp/index.html',context)
+
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'GeekShop - Каталог',
+        # 'products': Product.objects.all(),
+        # 'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'mainapp/products.html', context)
